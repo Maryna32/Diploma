75c8dff4fddbdb6e048d7d20cfdf49bba24e39f2
Maryna32 <marinaburda053@gmail.com>
2026-05-08 21:06:46 +0300

    feat: display more information for sections popular logs and people

--- tracker/components/home/PopularLogs.tsx
+++ tracker/components/home/PopularLogs.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 interface Log {
   id: number;
   title: string;
@@ -14,10 +16,12 @@ export default function PopularLogs({ logs }: { logs: Log[] }) {
       <h2 className="text-xl font-bold mb-4">Популярні записи</h2>
 
       {logs.map((log) => (
-        <div key={log.id} className="border p-4 rounded-xl mb-2">
-          <p className="font-semibold">{log.title}</p>
-          <p className="text-sm text-gray-500">Автор: @{log.user.username}</p>
-        </div>
+        <Link key={log.id} href={`/logs/${log.id}`}>
+          <div className="border p-4 rounded-xl mb-2 hover:bg-muted transition cursor-pointer">
+            <p className="font-semibold">{log.title}</p>
+            <p className="text-sm text-gray-500">Автор: @{log.user.username}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
--- tracker/components/home/PopularPeople.tsx
+++ tracker/components/home/PopularPeople.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 interface User {
   id: string;
@@ -22,22 +23,21 @@ function PopularPeople({ users }: { users: User[] }) {
 
       <div className="grid gap-4">
         {users.map((user) => (
-          <div
-            key={user.id}
-            className="flex items-center gap-3 p-4 border rounded-xl"
-          >
-            <Avatar className="w-12 h-12">
-              <AvatarImage src={user.avatarUrl || undefined} />
-              <AvatarFallback>{getInitials(user.username)}</AvatarFallback>
-            </Avatar>
+          <Link key={user.id} href={`/profile/${user.id}`}>
+            <div className="flex items-center gap-3 p-4 border rounded-xl hover:bg-muted transition cursor-pointer">
+              <Avatar className="w-12 h-12">
+                <AvatarImage src={user.avatarUrl || undefined} />
+                <AvatarFallback>{getInitials(user.username)}</AvatarFallback>
+              </Avatar>
 
-            <div>
-              <p className="font-semibold">@{user.username}</p>
-              <p className="text-sm text-gray-500">
-                Підписників: {user._count.followers}
-              </p>
+              <div>
+                <p className="font-semibold">@{user.username}</p>
+                <p className="text-sm text-gray-500">
+                  Підписників: {user._count.followers}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
