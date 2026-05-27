de19d34c05c8c1e6e7a6e396605312f8860205a8
Maryna32 <marinaburda053@gmail.com>
2026-05-27 12:21:36 +0300

    remove error

--- tracker/components/auth/AuthForm.tsx
+++ tracker/components/auth/AuthForm.tsx
@@ -129,7 +129,7 @@ export default function AuthForm() {
 
           <Button type="submit" className="w-full" disabled={loading}>
             {loading
-              ? "Вхідnpm run ..."
+              ? "Вхід..."
               : isSignUp
               ? "Зареєструватися"
               : "Увійти"}
